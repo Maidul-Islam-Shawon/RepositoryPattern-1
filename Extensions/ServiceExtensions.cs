f2930f3f1d90b7260551b6a4f609c9743dda99a0
Maidul-Islam-Shawon <mi_shawon@yahoo.com>
2020-11-17 18:18:32 +0000

    Add project files.

---
+++ Extensions/ServiceExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RP_Kudvenket.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+        }
+
+        public static void ConfugureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+
+        //public static void ConfigureLoggerService(this IServiceCollection services)
+        //{
+        //    services.AddSingleton<ILoggerManager, LoggerManager>
+        //}
+
+        public static void ConfigureMSSqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<RepositoryContext>(option =>
+                option.UseSqlServer(config.GetConnectionString("RepoPat-1-Context")));
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RP_Kudvenket.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
